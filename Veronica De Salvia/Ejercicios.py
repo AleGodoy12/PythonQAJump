ad554ecf4cb05efa9f55bf5e41c676f9ab3b97bb
verodesalvia <vdesalvia@arbusta.net>
2022-10-24 10:17:56 -0300

    Carga carpeta y ejercicios

---
+++ Veronica De Salvia/Ejercicios.py
@@ -0,0 +1,73 @@
+# Primera clase 17/10/2022
+
+#Horas = float(input("Por favor ingrese la cantidad de horas trabajadas: "))
+#Coste = 20
+#Paga = Horas * Coste
+#print(Paga)
+
+# Segunda clase 19/10/2022
+
+#Frutas = ["banana", "manzana", "pera"] #Lista
+#print(Frutas) 
+
+#Frutas.append("sandia") #Agrega a lista
+#print(Frutas) 
+
+#Frutas.pop(1) #Borra de lista
+#print(Frutas)
+
+#print(Frutas.count("pera")) #Cuantas veces se encuentra el elemento en la lista
+
+#tuple(Frutas)
+#print(Frutas)
+
+#Ejercicio 1.NumerosyString
+
+#1
+nombre = input("Por favor ingrese su nombre: ")
+print("Ahora estas en la matrix " + nombre)
+
+#2
+nrodecimale = float(input("Por favor ingrese un número con decimales: "))
+nroentero = int(input("Por favor ingrese un número entero: "))
+Suma = nrodecimale + nroentero
+print("El resultado de la suma es " + Suma)
+
+#3
+nro1 = float(input("Por favor ingresa un número: "))
+nro2 = float(input("Por favor ingresa otro número: "))
+suma = nro1 + nro2
+print("El resultado de la suma es" + suma)
+nro3 = float(input("Por favor ingrese un tercer número: "))
+multi = suma * nro3
+print("El resultado de la multiplicación es: " + multi)
+
+#4
+palabra1 = input("Por favor ingrese una palabra")
+palabra2 = input("Por favor ingrese otra palabra")
+concatenacion = palabra1 + " " + palabra2
+print(concatenacion)
+
+#5
+print("Hola mundo")
+
+#Ejercicio 2.ListasyTuplas
+
+#1
+Materias = ["Literatura", "Quimica", "Ingles"]
+print(Materias)
+
+#2
+ArtOficina = ["Engrapadora", "Lapicera", "Portapapeles"]
+ArtHomeOffice = ["Mate", "Post-it", "Tostadas"] #perdón, me daba gracia
+print(ArtOficina + " " + ArtHomeOffice)
+
+#3
+Lista = ["Esto", "es", "una", "lista"]
+tuple(Lista)
+
+#4
+
+#Ejercicios 3.Expresiones
+
+#Clase 3
