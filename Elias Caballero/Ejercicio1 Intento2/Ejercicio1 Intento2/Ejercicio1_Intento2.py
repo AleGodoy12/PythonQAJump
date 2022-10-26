62fda1ef2c81e421fca922f89d52cd13af3ad615
Elias Caballero <elias.caballero@arbusta.net>
2022-10-26 14:09:35 -0300

    Ejercicio 1 Elias Caballero

---
+++ Elias Caballero/Ejercicio1 Intento2/Ejercicio1 Intento2/Ejercicio1_Intento2.py
@@ -0,0 +1,49 @@
+#1
+nombre = input("Ingrese su nombre")
+#nombre = input()
+print("(Con la voz de Morfeo) " + nombre + ", si quieres vivir sigue mis instrucciones...")
+#-----------------------------------------------------------------------------------------------
+
+#2
+#print("Ingrese el primer número (con decimales) para la suma: ")
+num1 = float(input("Ingrese el primer número (con decimales) para la suma: "))
+
+#print("Ingrese el segundo número (sin decimales) para la suma: ")
+num2 = int(input("Ingrese el segundo número (sin decimales) para la suma: "))
+
+suma = num1 + num2
+print("La suma de los dos número es: " + suma)
+#-----------------------------------------------------------------------------------------------
+
+#3
+#print("Ingrese 2 números para la suma: ")
+
+#print("primer número: ")
+num3 = int(input("Ingrese 2 números para la suma: "))
+
+#print("Segundo número: ")
+num4 = int(input("Segundo número: "))
+
+suma2 = num3 + num4
+print("La suma de los dos número es: " + suma2)
+
+#print("Ingrese un número para la multiplicación con la suma anterior: ")
+num5 = int(input("Ingrese un número para la multiplicación con la suma anterior: "))
+
+multi = suma2 * num5
+print("La multiplicación de la suma por " + num5 + " es : " + multi)
+#-----------------------------------------------------------------------------------------------
+
+#4
+#print ("ingresa la primera palabra: ")
+pal1 = input("ingresa la primera palabra: ")
+
+#print ("ingresa la segunda palabra: ")
+pal2 = input("ingresa la segunda palabra: ")
+
+print (pal1 + pal2 + " jaja, muy gracioso.")
+#-----------------------------------------------------------------------------------------------
+
+#5
+print ("Hola mundo")
+#-----------------------------------------------------------------------------------------------
