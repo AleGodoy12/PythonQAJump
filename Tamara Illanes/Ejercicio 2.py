9dedc54de2e0ec6181dbd95e393ef3f5f40c5167
unknown <tillanes@arbusta.net>
2022-10-28 11:23:16 -0300

    ejercicios tamara illanes

---
+++ Tamara Illanes/Ejercicio 2.py
@@ -0,0 +1,27 @@
+# #1
+# nombre = input ("ingresa tu nombre: ")
+# print("Ahora estas en la matrix," + nombre)
+
+# #2
+# usuario1 =float(input("ingrese un numero decimal: "))
+# usuario2 = int(input("ingrese un numero entero:"))
+# suma = float(usuario1+usuario2)
+# print("Resultado de la suma:" , + suma)
+
+#3
+#numero1 =int(input("ingrese el primer numero: "))
+#numero2 = int(input("ingrese el sugundo numero:"))
+#suma = numero1+ numero2
+#print("el resultado de la suma:",suma)
+#numero3 =int(input("ingrese el tercer numero:"))
+#multiplicacion = suma * numero3
+#print("Resultado de la multiplicacion:", multiplicacion)
+
+#4
+#Letra1 = input("ingrese la primera palabra:" )
+#Letra2 = input("ingrese la segunda palabra:")
+#Letra3 = Letra1 +" "+Letra2
+#print("Su frase es:", Letra3)
+
+#5
+print("Hola mundo")
