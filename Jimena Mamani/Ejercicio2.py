3521840a97bc63ccd49131100e33237380511cd7
Jimena Mamani <jmamani@arbusta.net>
2022-10-28 11:39:38 -0300

    ejercicios jimena

---
+++ Jimena Mamani/Ejercicio2.py
@@ -0,0 +1,23 @@
+#Ejercico 1
+#lista =  ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+#print (lista)
+
+#Ejercico 2
+#lista1 =  ["Lapicera", "Goma", "Cafetera", "Block de notas"]
+#lista2 = ["Anteojos", "Escritorio","Auriculares"]
+#lista3 = lista1 + lista2 
+#print ("Las listas son:", lista3)
+
+#Ejercico 3
+#lista = ["Lapicera", "Goma", "Cafetera", "Block de notas", "245"] 
+#tuple (lista)
+#print ("tu lista es: ", lista)
+
+#Ejercico 4
+#chocolates = ["negros","blancos", "amargos"]
+#chocolates .append("dulces")
+#print(chocolates)
+
+#len(chocolates)
+#chocolates.pop()
+#chocolates.count("negro")
--- Jimena Mamani/Ejercicios1.py
+++ Jimena Mamani/Ejercicios1.py
@@ -1,2 +1,29 @@
 #Ejercicio 1
+# nombre = input ("ingresa tu nombre:")
+# print ("Ahora estas en la matrix,"+ nombre )
 
+#Ejercicio 2
+# usuario1 = float (input ("ingrese numero con punto:"))
+# usuario2 = int ( input("ingrese un numero sin punto:"))
+# suma = float(usuario1+usuario2)
+# print ("tu reasultado es:" , suma)
+
+#Ejercicio 3
+
+#persona1 = int (input("ingresa tu numero de la suerte:"))
+#persona2 = int (input("ingrese el numero que no queres:"))
+#suma = persona1 + persona2
+#print ("el resultado de esto es:" ,suma)
+#persona3 = int (input("ingrese otro numero de la suerte:"))
+#multiplicacion = suma * persona3 
+#print ("tu numero de la suerte es:", multiplicacion)
+
+#Ejercicio 4
+
+#frase1 = input ("ingrese su primera frase:")
+#frase2 = input ("ingrese su segunda frase:")
+#frase3 = (frase1 + " " + frase2)
+#print ("su frase es:", frase3)
+
+#Ejercicio 5
+#print ("Hola mundo")
